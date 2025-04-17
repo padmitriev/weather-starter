32d1e9a10c73b298ed9a7b76354ef8fa43142e2f
padmitriev <dmkolos@gmail.com>
2025-04-17 20:25:34 +0300

    init commit

---
+++ src/main/java/dpa/weather/weatherstarter/service/implementation/WeatherServiceImpl.java
@@ -0,0 +1,19 @@
+package dpa.weather.weatherstarter.service.implementation;
+
+import dpa.weather.weatherstarter.client.WeatherClient;
+import dpa.weather.weatherstarter.responce.WeatherResponse;
+import dpa.weather.weatherstarter.service.WeatherService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class WeatherServiceImpl implements WeatherService {
+
+    private final WeatherClient weatherClient;
+
+    @Override
+    public WeatherResponse getWeatherForCity(String city) {
+        return weatherClient.getWeatherByCity(city);
+    }
+}
