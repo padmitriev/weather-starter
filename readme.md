f69cdda9435af45edad1808322a60dcdce0ea5cb
padmitriev <dmkolos@gmail.com>
2025-04-17 20:36:53 +0300

    Revert "init commit"
    This reverts commit 32d1e9a10c73b298ed9a7b76354ef8fa43142e2f.

--- readme.md
+++
@@ -1,6 +0,0 @@
-Стартер для получения погоды через API Openweathermap
-https://api.openweathermap.org/data/2.5/weather
-
-Информация о погоде по названию города
-
-endpoint: /weather/{city}
--- src/main/java/dpa/weather/weatherstarter/service/WeatherService.java
+++
@@ -1,7 +0,0 @@
-package dpa.weather.weatherstarter.service;
-
-import dpa.weather.weatherstarter.responce.WeatherResponse;
-
-public interface WeatherService {
-    WeatherResponse getWeatherForCity(String city);
-}
