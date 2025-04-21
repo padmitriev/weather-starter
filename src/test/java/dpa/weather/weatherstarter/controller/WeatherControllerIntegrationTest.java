b4fec65c9c512722251c27225ce46defdd2b76cf
padmitriev <dmkolos@gmail.com>
2025-04-21 15:15:11 +0300

    add WeatherClientIntegrationTest

---
+++ src/test/java/dpa/weather/weatherstarter/controller/WeatherControllerIntegrationTest.java
@@ -0,0 +1,58 @@
+package dpa.weather.weatherstarter.controller;
+
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
+import dpa.weather.weatherstarter.response.WeatherResponse;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class WeatherControllerIntegrationTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @RegisterExtension
+    static WireMockExtension wireMockServer = WireMockExtension.newInstance()
+            .options(wireMockConfig().dynamicPort())
+            .build();
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("weather.api-url", wireMockServer::baseUrl);
+        registry.add("weather.api-key", () -> "test-key");
+    }
+
+    @Test
+    void getWeather_IntegrationTest() {
+        // Настраиваем WireMock
+        wireMockServer.stubFor(WireMock.get(urlPathEqualTo("/data/2.5/weather/"))
+                .withQueryParam("q", equalTo("Paris"))
+                .willReturn(aResponse()
+                        .withBody("{\"city\":\"Paris\",\"temperature\":285.0,\"description\":\"Sunny\"}")
+                        .withStatus(200)));
+
+        ResponseEntity<WeatherResponse> response = restTemplate.getForEntity(
+                "/weather/Paris", WeatherResponse.class);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals("Paris", response.getBody().getCity());
+
+        wireMockServer.verify(1,
+                getRequestedFor(urlPathEqualTo("/data/2.5/weather"))
+                        .withQueryParam("q", equalTo("Paris")));
+    }
+}
