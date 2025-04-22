e9b7cd01e274d15c2541b105535a9253aaeccf65
padmitriev <dmkolos@gmail.com>
2025-04-22 22:17:32 +0300

    update

--- src/main/java/dpa/weather/weatherstarter/service/implementation/WeatherServiceImpl.java
+++ src/main/java/dpa/weather/weatherstarter/service/implementation/WeatherServiceImpl.java
@@ -1,7 +1,6 @@
 package dpa.weather.weatherstarter.service.implementation;
 
 import dpa.weather.weatherstarter.client.WeatherClient;
-import dpa.weather.weatherstarter.client.implementation.WeatherClientImpl;
 import dpa.weather.weatherstarter.response.WeatherResponse;
 import dpa.weather.weatherstarter.service.WeatherService;
 import lombok.RequiredArgsConstructor;
